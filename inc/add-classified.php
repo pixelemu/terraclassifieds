91ff231b422019982fc52de55cb839a7a2cb7797
maciej.wolpiuk <maciej.wolpiuk@gmail.com>
2021-03-06 13:38:06 +0100

    Dodanie funkcji tworzące posty do obsługi procesu płatności PayPal oraz listy płatności na froncie dla użytkowników.

--- inc/add-classified.php
+++ inc/add-classified.php
@@ -226,3 +226,106 @@ function terraclassifieds_favourite_ads_page() {
 	return true;
 }
 terraclassifieds_favourite_ads_page();
+
+//PayPal Payment Success Page
+function terraclassifieds_paypal_payment_success_page() {
+	$post_slug = terraclassifieds_get_option( '_tc_monetizing_paypal_payment_return_successful_url', 'paypal-payment-success' );
+
+	if( terraclassifieds_post_by_slug( $post_slug, 'page') ) {
+		return;
+	}
+
+    // Create post object
+    $my_post = array(
+      'post_title'    => wp_strip_all_tags( 'Terraclassifieds - PayPal Payment Success' ),
+      'post_content'  => '[terraclassifieds_paypal_payment_success]',
+      'post_status'   => 'publish',
+      'post_author'   => 1,
+	  'post_type'     => 'page',
+	  'post_name'     => $post_slug,
+    );
+
+    // Insert the post into the database
+	wp_insert_post( $my_post );
+
+	return true;
+}
+
+terraclassifieds_paypal_payment_success_page();
+
+//PayPal Payment Cancel Page
+function terraclassifieds_paypal_payment_cancel_page() {
+	$post_slug = terraclassifieds_get_option( '_tc_monetizing_paypal_payment_return_cancel_url', 'paypal-payment-cancel' );
+
+	if( terraclassifieds_post_by_slug( $post_slug, 'page') ) {
+		return;
+	}
+
+    // Create post object
+    $my_post = array(
+      'post_title'    => wp_strip_all_tags( 'Terraclassifieds - PayPal Payment Cancel' ),
+      'post_content'  => '[terraclassifieds_paypal_payment_cancel]',
+      'post_status'   => 'publish',
+      'post_author'   => 1,
+	  'post_type'     => 'page',
+	  'post_name'     => $post_slug,
+    );
+
+    // Insert the post into the database
+	wp_insert_post( $my_post );
+
+	return true;
+}
+
+terraclassifieds_paypal_payment_cancel_page();
+
+
+//PayPal Payment Notify Page
+function terraclassifieds_paypal_payment_notify_page() {
+	$post_slug = terraclassifieds_get_option( '_tc_monetizing_paypal_payment_return_notify_url', 'paypal-payment-notify' );
+
+	if( terraclassifieds_post_by_slug( $post_slug, 'page') ) {
+		return;
+	}
+
+    // Create post object
+    $my_post = array(
+      'post_title'    => wp_strip_all_tags( 'Terraclassifieds - PayPal Payment Notify' ),
+      'post_content'  => '[terraclassifieds_paypal_payment_notify]',
+      'post_status'   => 'publish',
+      'post_author'   => 1,
+	  'post_type'     => 'page',
+	  'post_name'     => $post_slug,
+    );
+
+    // Insert the post into the database
+	wp_insert_post( $my_post );
+
+	return true;
+}
+
+terraclassifieds_paypal_payment_notify_page();
+
+//Payments List Page
+function terraclassifieds_payments_list_page() {
+	$post_slug = terraclassifieds_get_option( '_tc_slug_my_payments', 'my-payments' );
+
+	if( terraclassifieds_post_by_slug( $post_slug, 'page') ) {
+		return;
+	}
+
+    // Create post object
+    $my_post = array(
+      'post_title'    => wp_strip_all_tags( 'Terraclassifieds - My payments' ),
+      'post_content'  => '[terraclassifieds_my_payments]',
+      'post_status'   => 'publish',
+      'post_author'   => 1,
+	  'post_type'     => 'page',
+	  'post_name'     => $post_slug,
+    );
+
+    // Insert the post into the database
+	wp_insert_post( $my_post );
+	return true;
+}
+terraclassifieds_payments_list_page();
