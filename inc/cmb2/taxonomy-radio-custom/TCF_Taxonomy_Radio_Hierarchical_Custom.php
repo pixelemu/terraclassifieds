dd7404c7a538de58ec8ace4fc61e808c74bf2fa7
maciej.wolpiuk <maciej.wolpiuk@gmail.com>
2021-03-07 15:22:57 +0100

    Dostosowanie do opłat za kategorie.

--- inc/cmb2/taxonomy-radio-custom/TCF_Taxonomy_Radio_Hierarchical_Custom.php
+++ inc/cmb2/taxonomy-radio-custom/TCF_Taxonomy_Radio_Hierarchical_Custom.php
@@ -48,6 +48,40 @@ class TCF_Taxonomy_Radio_Hierarchical_Custom extends CMB2_Type_Taxonomy_Radio_Hi
 		), $args );
 		if($a['name'] == '_tc_category'){
 			$taxonomy = 'ad_category';
+			$price_for_charging_ads = '';
+			$charing_for_add_ads = terraclassifieds_get_option('_tc_monetizing_charging_for_adding_ads_price','free');
+			$currency = terraclassifieds_get_option( '_tc_advert_currency', '$' );
+			$unit_position = (int) terraclassifieds_get_option( '_tc_unit_position', 1 );
+			if ($charing_for_add_ads === 'fixed') {
+				$price_for_charging_ads = floatval(terraclassifieds_get_option('_tc_monetizing_charging_for_adding_ads_price_fixed','0'));
+				if (intval($price_for_charging_ads) == 0) {
+					$price_for_charging_ads_text = ' (Free)';
+				}else{
+					if (!$unit_position) {
+						$price_for_charging_ads_text = ' ('.$currency.' '.terraclassifiedsPriceFormat($price_for_charging_ads,1).')';
+					}else{
+						$price_for_charging_ads_text = ' ('.terraclassifiedsPriceFormat($price_for_charging_ads,1).' '.$currency.')';
+					}
+				}
+			}elseif($charing_for_add_ads === 'per_category') {
+				$terms = get_terms(array('taxonomy' => $taxonomy,'hide_empty' => false, 'parent' => 0));
+				if (!empty($terms) && is_array($terms)) {
+					$prices_for_charging_ads = array();
+					foreach($terms as $key=>$term) {
+						$prices_for_charging_ads[$term->term_id] = floatval(terraclassifieds_get_option('_tc_monetizing_charging_for_adding_ads_price_per_category_charging_price_'.$term->term_id,'0'));
+						if(get_term_children( $term->term_id, $taxonomy)){
+							$term_children = get_term_children( $term->term_id, $taxonomy);
+							if (!empty($term_children) && is_array($term_children)) {
+								foreach($term_children as $child_key=>$child_item) {
+									$parent_name = $term->name;
+									$child_term = get_term($child_item, $taxonomy);
+									$prices_for_charging_ads[$child_term->term_id] = floatval(terraclassifieds_get_option('_tc_monetizing_charging_for_adding_ads_price_per_category_charging_price_'.$child_term->term_id,'0'));
+								}
+							}
+						}
+					}
+				}
+			}
 			$terms = get_terms($taxonomy, array("hide_empty" => false)); // Get all terms of a taxonomy
 			foreach ( $terms as $term ) {
 				$cat = get_term_by('slug', $a['value'], $taxonomy);
@@ -66,9 +100,41 @@ class TCF_Taxonomy_Radio_Hierarchical_Custom extends CMB2_Type_Taxonomy_Radio_Hi
 					
 					if(get_term_children( $cat_id, $taxonomy )){
 						$is_parent = 'class="parent"';
-						return sprintf( "\t" . '<li %s><span><input%s/><label for="%s">%s<span>%s</span></label></span>' . "\n", $is_parent, $this->concat_attrs( $a, array( 'label' ) ), $a['id'], $image, $a['label']);
+						if($charing_for_add_ads === 'per_category') {
+							$price_for_charging_ads = $prices_for_charging_ads[$this->term->term_id];
+							if (intval($price_for_charging_ads) == 0) {
+								$price_for_charging_ads_text = ' (Free)';
+							}else{
+								if (!$unit_position) {
+									$price_for_charging_ads_text = ' ('.$currency.' '.terraclassifiedsPriceFormat($price_for_charging_ads,1).')';
+								}else{
+									$price_for_charging_ads_text = ' ('.terraclassifiedsPriceFormat($price_for_charging_ads,1).' '.$currency.')';
+								}
+							}
+						}
+						if (!is_admin()) {
+							return sprintf( "\t" . '<li %s><span><input%s/><label for="%s">%s<span data-category="%s" data-id-category="%s" data-category-price="%s">%s</span></label></span>' . "\n", $is_parent, $this->concat_attrs( $a, array( 'label' ) ), $a['id'], $image, $a['label'], $this->term->term_id, $price_for_charging_ads, $a['label'].'<br/>'.$price_for_charging_ads_text);
+						}else{
+							return sprintf( "\t" . '<li %s><span><input%s/><label for="%s">%s<span data-category="%s" data-id-category="%s" data-category-price="%s">%s</span></label></span>' . "\n", $is_parent, $this->concat_attrs( $a, array( 'label' ) ), $a['id'], $image, $a['label'], $this->term->term_id, $price_for_charging_ads, $a['label'].$price_for_charging_ads_text);
+						}
 					} else {
-						return sprintf( "\t" . '<li %s><span><input%s/><label for="%s">%s<span>%s</span></label></span></li>' . "\n", $is_parent, $this->concat_attrs( $a, array( 'label' ) ), $a['id'], $image, $a['label']);
+						if($charing_for_add_ads === 'per_category') {
+							$price_for_charging_ads = $prices_for_charging_ads[$this->term->term_id];
+							if (intval($price_for_charging_ads) == 0) {
+								$price_for_charging_ads_text = ' (Free)';
+							}else{
+								if (!$unit_position) {
+									$price_for_charging_ads_text = ' ('.$currency.' '.terraclassifiedsPriceFormat($price_for_charging_ads,1).')';
+								}else{
+									$price_for_charging_ads_text = ' ('.terraclassifiedsPriceFormat($price_for_charging_ads,1).' '.$currency.')';
+								}
+							}
+						}
+						if (!is_admin()) {
+							return sprintf( "\t" . '<li %s><span><input%s/><label for="%s">%s<span data-category="%s" data-id-category="%s" data-category-price="%s">%s</span></label></span></li>' . "\n", $is_parent, $this->concat_attrs( $a, array( 'label' ) ), $a['id'], $image, $a['label'], $this->term->term_id, $price_for_charging_ads, $a['label'].'<br/>'.$price_for_charging_ads_text);
+						}else{
+							return sprintf( "\t" . '<li %s><span><input%s/><label for="%s">%s<span data-category="%s" data-id-category="%s" data-category-price="%s">%s</span></label></span></li>' . "\n", $is_parent, $this->concat_attrs( $a, array( 'label' ) ), $a['id'], $image, $a['label'], $this->term->term_id, $price_for_charging_ads, $a['label'].$price_for_charging_ads_text);
+						}
 					}
 				}
 			}
